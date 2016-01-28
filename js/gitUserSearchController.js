736705c15ab825c128fc4cbdceb7ee6ea9d26f0a
tmgree <tmgree@outlook.com>
2016-01-28 13:45:09 +0000

    secrets added

--- js/gitUserSearchController.js
+++ js/gitUserSearchController.js
@@ -1,12 +1,13 @@
 githubUserSearch.controller('GitUserSearchController', ['$resource', function($resource) {
   var self = this;
-  var access_token = 'd3acfde1071f1c70da3aa8335910c6648a16f78e'
-  var searchResource = $resource('https://api.github.com/search/users'+ "?access_token=" + access_token);
+  var searchResource = $resource('https://api.github.com/search/users');
   self.searchTerm = 0;
 
   self.doSearch = function(){
     self.searchResult = searchResource.get(
-  { q: self.searchTerm }
+
+  { access_token: access_token,
+    q: self.searchTerm }
 );
   };
 }]);
--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -14,18 +14,33 @@ describe('GitUserSearchController', function(){
   describe('when searching for a user', function(){
 
     var httpBackend;
-  beforeEach(inject(function($httpBackend) {
-    httpBackend = $httpBackend;
-    httpBackend
-      .when("GET", "https://api.github.com/search/users?q=hello")
-      .respond(
-        { items: items }
-      );
-  }));
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend;
+      httpBackend
+        .when("GET", "https://api.github.com/search/users?access_token=d3acfde1071f1c70da3aa8335910c6648a16f78e&q=hello")
+        .respond(
+          { items: items }
+        );
+    }));
+
+    var items = [
+      {
+        "login": "tansaku",
+        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+        "html_url": "https://github.com/tansaku"
+      },
+      {
+        "login": "stephenlloyd",
+        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+        "html_url": "https://github.com/stephenlloyd"
+      }
+    ];
+
 
     it('displays search results', function() {
       ctrl.searchTerm = "hello";
       ctrl.doSearch();
+      httpBackend.flush();
      expect(ctrl.searchResult.items).toEqual(items);
     });
   });
